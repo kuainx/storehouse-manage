bd1b0bf2a41a51b9ca63f66e10bfcd03b2eeaed6
KUAI <2542862169@qq.com>
2023-05-18 11:26:02 +0800

    fix csrf

--- backend/storehouse/settings.py
+++ backend/storehouse/settings.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
     'log',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
